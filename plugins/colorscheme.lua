4cb2ca79645846cde7eabab1d6d003122d5d2095
fudo-mirror <fudo.mirror@gmail.com>
2024-04-03 09:52:16 -0400

    Updated configs for nvim

---
+++ plugins/colorscheme.lua
@@ -0,0 +1,13 @@
+-- Colorscheme for Solarized-Osaka
+return {
+  {
+    "craftzdog/solarized-osaka.nvim",
+    lazy = true,
+    priority = 1000,
+    opts = function()
+      return {
+        transparent = true,
+      }
+    end,
+  },
+}
